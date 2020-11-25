cc4c2fb4ef76f85b12a947b864124d75ec3add3a
DevinBurts <devinburts0@gmail.com>
2020-11-25 10:13:43 -0600

    milestone2.2

--- New Unity Project/Assets/Scripts/AIController.cs
+++ New Unity Project/Assets/Scripts/AIController.cs
@@ -6,8 +6,17 @@ public class AIController : MonoBehaviour
 {
     // Enum for the behavior of the AI
     public enum AttackMode {chase, flee };
+    public enum Behavior {berserk, heal, damageIncrease, normal}
+    public Behavior behavior;
     public AttackMode attackMode;
-    
+
+    // List for waypoints
+    public List<Transform> waypoints;
+
+    // Variable for the current waypoint the tank is heading to
+    public int currentWaypoint;
+    public float minDistance;
+
     // Variable to access the TankMotor script
     private TankMotor motor;
     
@@ -29,11 +38,35 @@ public class AIController : MonoBehaviour
     public float avoidanceTime;
     private float exitTime;
     private float originalSpeed;
+    private float originalTime;
 
     // Variable for the detection range of the tanks
     public float visionRange;
     public float hearingRange;
     public RaycastHit hitInfo;
+
+    // Variable for direction to the player
+    public Vector3 playerDirection;
+
+    // Variable for direction away from player
+    public Vector3 fleeDirection;
+
+    // Variable for direction to the next waypoint
+    public Vector3 targetDirection;
+
+    // Variable for the player
+    public GameObject player;
+
+    // Variable for the state of the tank
+    public string state;
+
+    // Variables for the angle between the tank and a target
+    public float angleToPlayer;
+    public float angleToWaypoint;
+
+    // Variables for the field of vision
+    public float FOV;
+    private float mirroredFOV;
 
     // Start is called before the first frame update
     void Start()
@@ -52,6 +85,12 @@ public class AIController : MonoBehaviour
         gmCaller.activeEnemies.Add(gameObject);
 
         avoidanceStage = 0;
+
+        originalTime = avoidanceTime;
+
+        mirroredFOV = 180 - FOV;
+
+        state = "Patrol";
     }
 
     // Update is called once per frame
@@ -74,59 +113,184 @@ public class AIController : MonoBehaviour
         }
         if (CanMove(motor.moveSpeed))
         {
-            motor.Forwards();
+            //if (state == "Patrol")
+            //{
+                Patrol();
+            //}
         }
         else
         {
             motor.moveSpeed = 0;
-            ObstacleAvoidance();
+            avoidanceStage = 1;
+            ObstacleAvoidance(state);
         }
     }
     
-     bool CanMove(float speed)
+    bool CanMove(float speed)
     {
         RaycastHit hit;
         // Check if there is something in front of the tank
-        if(Physics.Raycast(transform.position, transform.forward, out hit, speed *2f))
+        if(Physics.Raycast(transform.position, transform.forward, out hit, speed))
         {
             // Check if the tank is not seeing the player
             if (!hit.collider.CompareTag("Player"))
             {
-                Debug.Log("Obstacle detected!");
-                avoidanceStage = 1;
                 return false;
             }
             else
-            {
-
-                Debug.Log("Player detected!");
+            {              
                 return true;
             }
         }
+        // There is nothing infront of the tank
         else
         {
-            Debug.Log("I can move");
             return true;
         }
     }
 
-    void ObstacleAvoidance()
+    void ObstacleAvoidance(string originalState)
     {
+        avoidanceTime = originalTime;
         while (avoidanceStage == 1)
         {
-            // Rotate for 2 secs until the tank can move again
-            motor.RotateRight();
-           // avoidanceTime = 2.0f;
-            //avoidanceTime -= Time.deltaTime;
-            //if (avoidanceTime <= 0)
-          //  {
-                if (CanMove(motor.moveSpeed))
-                {
-                    motor.moveSpeed = originalSpeed;
-                    avoidanceStage = 0;
-                }
-            //}
+            // Countdown
+            avoidanceTime -= Time.deltaTime;
+            if (avoidanceTime > 0)
+            {
+                // Rotate until the tank can move again
+                motor.RotateRight();
+            }
+            else
+            {
+                avoidanceStage = 0;
+            } 
+        }
+        while (avoidanceStage == 0)
+        {
+            ChangeState(originalState);
         }
     }
     
-}
+    // Behaviors
+
+    void Healing()
+    {
+
+    }
+
+    void DamageIncrease()
+    {
+
+    }
+
+    void Berserk()
+    {
+
+    }
+
+    void Chase()
+    {
+
+    }
+
+    void Flee()
+    {
+
+    }
+
+    void Patrol()
+    {
+        //
+        motor.moveSpeed = originalSpeed;
+        // Angle between the front of the tank and the current waypoint
+        angleToWaypoint = Vector3.Angle(transform.forward, waypoints[currentWaypoint].position);
+        // Rotate towards the current waypoint
+        RotateTo(waypoints[currentWaypoint].position, motor.turnSpeed);
+        // If the tank has its waypoint in view
+        if (angleToWaypoint <= FOV || angleToWaypoint <= mirroredFOV)
+        {
+            // Move towards the waypoint
+            MoveTo(waypoints[currentWaypoint].position, motor.moveSpeed);
+        }
+        // If the tank moves close enough to the waypoint, set their target to the next waypoint
+        if (Vector3.Distance(waypoints[currentWaypoint].position, transform.position) <= minDistance)
+        {
+            currentWaypoint++;
+        }
+        // If the tank reached the last checkpoint, go through the route again 
+        if (currentWaypoint == waypoints.Count)
+        {
+            currentWaypoint = 0;
+        }
+    }
+
+    void ChangeState(string newState)
+    {
+        state = newState;
+    }
+
+    bool CanHear(GameObject chosenPlayer)
+    {
+        // Check if the player is alive
+        if (chosenPlayer != null)
+        {
+            // Check if the player is within hearing range
+            if (Vector3.Distance(chosenPlayer.transform.position, transform.position) <= hearingRange)
+            {
+                // The player can be heard
+                return true;
+            }
+            else
+            {
+                // The player can't be heard
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool CanSee(GameObject chosenPlayer)
+    {
+        // Check if the player is alive
+        if (chosenPlayer != null)
+        {
+            // Get the direction to the player
+            angleToPlayer = Vector3.Angle(transform.forward, playerDirection);
+            // Check if the player is within the tank's field of vision
+            if (angleToPlayer <= FOV || angleToPlayer <= mirroredFOV )
+            {
+                // The tank can see the player
+                return true;
+            }
+            else
+            {
+                // The player is not withing the FOV
+                return false;
+            }
+        }
+        else
+        {
+            // The player is not alive
+            return false;
+        }
+    }
+    public void RotateTo(Vector3 target, float speed)
+    {
+        // Get the direction of the target
+        targetDirection = target - transform.position;
+        // Rotate towards the target over time
+        Quaternion rotation = Quaternion.LookRotation(targetDirection);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, speed * Time.deltaTime);
+    }
+
+    public void MoveTo(Vector3 target, float speed)
+    {
+        // Move towards the target
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * 0.5f * Time.deltaTime);
+    }
+
+    }
